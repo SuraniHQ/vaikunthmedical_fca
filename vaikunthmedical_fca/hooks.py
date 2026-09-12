cdbde6d0a41c171c7986d5188358688c6be22f73
suraniharsh <harshsurani00@gmail.com>
2026-09-13 00:49:11 +0530

    Revert "feat: Discount % becomes Amount-only too, Rate never touched by either"
    This reverts commit 76b6b2c113d7e60cacf56fb7f6a855262d6f8dea.

--- vaikunthmedical_fca/hooks.py
+++ vaikunthmedical_fca/hooks.py
@@ -155,19 +155,15 @@ doc_events = {
 		"before_validate": [
 			"vaikunthmedical_fca.purchase_receipt.apply_expiry_month_year",
 			"vaikunthmedical_fca.purchase_receipt.snapshot_typed_batch_no",
-			"vaikunthmedical_fca.scm.neutralize_discount_percentage",
-		],
-		"validate": [
-			"vaikunthmedical_fca.purchase_receipt.ensure_batch_with_expiry",
-			"vaikunthmedical_fca.scm.apply_scm_and_discount_to_amount",
+			"vaikunthmedical_fca.scm.apply_scm_to_rate",
 			"vaikunthmedical_fca.scm.set_total_scm_amount",
 		],
+		"validate": "vaikunthmedical_fca.purchase_receipt.ensure_batch_with_expiry",
 		"on_submit": "vaikunthmedical_fca.purchase_receipt.sync_batch_expiry_and_selling_price",
 	},
 	"Purchase Invoice": {
-		"before_validate": "vaikunthmedical_fca.scm.neutralize_discount_percentage",
-		"validate": [
-			"vaikunthmedical_fca.scm.apply_scm_and_discount_to_amount",
+		"before_validate": [
+			"vaikunthmedical_fca.scm.apply_scm_to_rate",
 			"vaikunthmedical_fca.scm.set_total_scm_amount",
 		],
 	},
--- vaikunthmedical_fca/public/js/scm.js
+++ vaikunthmedical_fca/public/js/scm.js
@@ -1,14 +1,14 @@
-function vmfca_recompute_amount(frm, cdt, cdn) {
+function vmfca_apply_scm(frm, cdt, cdn) {
 	const row = locals[cdt][cdn];
-	const base_amount = flt(row.qty) * flt(row.rate);
-	const discount_value = (base_amount * flt(row.discount_percentage)) / 100.0;
+	const qty = flt(row.qty) || 1;
+	const price_list_rate = flt(row.price_list_rate) || flt(row.rate);
+	const rate_after_discount = price_list_rate * (1 - flt(row.discount_percentage) / 100.0);
 
-	frappe.model.set_value(cdt, cdn, "amount", base_amount - discount_value - flt(row.custom_scm_amount));
+	frappe.model.set_value(cdt, cdn, "rate", rate_after_discount - flt(row.custom_scm_amount) / qty);
 }
 
 ["Purchase Receipt", "Purchase Invoice"].forEach((parent_doctype) => {
 	frappe.ui.form.on(`${parent_doctype} Item`, {
-		custom_scm_amount: vmfca_recompute_amount,
-		discount_percentage: vmfca_recompute_amount,
+		custom_scm_amount: vmfca_apply_scm,
 	});
 });
